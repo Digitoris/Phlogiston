abb7f36efde4ecde3bfc697fc11e6ba42d058918
Digitoris <vanderburgh.jesse@gmail.com>
2018-07-12 01:11:38 +0200

    verified_now

--- src/main/java/digitas/phlogiston/world/WorldGenPhlogiston.java
+++ src/main/java/digitas/phlogiston/world/WorldGenPhlogiston.java
@@ -3,6 +3,7 @@ package digitas.phlogiston.world;
 import java.util.Random;
 
 import cpw.mods.fml.common.IWorldGenerator;
+import net.minecraft.init.Blocks;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
 
@@ -21,14 +22,14 @@ public class WorldGenPhlogiston implements IWorldGenerator {
 			int xBase = chunkX*16;
 			int zBase = chunkZ*16;
 
-			/*
+			
 			for (int i=0; i<16; i++) {
 				int x = xBase + i;
 				for (int j=0; j<16; j++) {
 					world.setBlock(x,200,zBase+j,Blocks.diamond_block,0,2);
 				}
 			}
-			*/
+			
 			
 		}
 
